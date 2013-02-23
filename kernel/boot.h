657cf9959d520a6e97f60271aeca999f207de81e
n <wo_atmark@s9.dion.ne.jp>
2013-02-23 15:39:54 +0900

    fix 64

---
+++ kernel/boot.h
@@ -0,0 +1,8 @@
+#ifndef QO3_KERNEL_BOOT_H
+#define QO3_KERNEL_BOOT_H
+
+struct boot_param {
+	void *heap_start;
+};
+
+#endif
---
+++ kernel/numa.h
@@ -0,0 +1,7 @@
+#ifndef QO3_KERNEL_NUMA_H
+#define QO3_KERNEL_NUMA_H
+
+
+#define current_nodeid() 0
+
+#endif
